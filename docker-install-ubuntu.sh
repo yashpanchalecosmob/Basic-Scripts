c91793304c6486ccfdbea308883ceda86c1248f1
yashpanchalecosmob <75319601+yashpanchalecosmob@users.noreply.github.com>
2021-01-25 13:11:24 +0530

    Create docker-install-ubuntu.sh
    Docker and docker-compose installation

---
+++ docker-install-ubuntu.sh
@@ -0,0 +1,32 @@
+apt-get update 
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update && sudo apt-get install -y containerd.io=1.2.13-2 docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
+
+sudo mkdir /etc/docker
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker
+
+cd /usr/local/
+wget -o docker-compose https://github.com/docker/compose/releases/download/1.28.0/docker-compose-Linux-x86_64
+chmod +x docker-compose
